a5da46cc6ca617c0f5bb9e14b713d4e0a5742fe5
MULARI <mulari@project.local>
2026-04-13 15:12:25 +0700

    Update API_URL v58

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   LIFF_ID: '2009422664-jGGTOgjJ',
-  API_URL: 'https://script.google.com/macros/s/AKfycbxelUZ5qF1ydVClZPiP1k0R1Wyoz31-mttVWi8uSqcguksJK9zcWdYTd3fODu1cRkQfQA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwbm_XNLYT4AvOzTKT6Kuk0OZtaxim5RQl--0dtRbGve2VC78tXJmZACOhtlIbeZcSzzA/exec',
   SHOP_NAME: 'MULARI'
 };
 
